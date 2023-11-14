2e8baa0be58bd7125a75c7f3ebc2d0d8c61ef9bd
bennydan7 <owusub243@gmail.com>
2023-11-14 05:29:13 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select a media stream, pass to video element
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (erorr) {
+    // Catch Erro here
+    console.log('whoops, error here', console.error());
+  }
+}
+
+button.addEventListener('click', async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in picture
+  await videoElement.requestPictureInPicture();
+
+  // Reset Button
+  button.disabled = false;
+});
+
+//On load
+selectMediaStream();
